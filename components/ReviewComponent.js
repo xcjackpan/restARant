7269fb9be85c685d470b91289ec5a5b1519e387a
Katie Zhan <rqzhan@uwaterloo.ca>
2018-03-04 04:34:22 -0500

    fixed interface

--- components/ReviewComponent.js
+++ components/ReviewComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import {StyleSheet, Text, View } from 'react-native';
 
 export default class ReviewComponent extends Component {
 
@@ -9,11 +9,34 @@ export default class ReviewComponent extends Component {
 
 	render() {
 		return (
-			<View>
-				<Text> {this.props.author_name} </Text>
-				<Text> {this.props.text} </Text>
+			<View style={styles.overall}>
+				<Text style={styles.name}> {this.props.author_name} </Text>
+				<Text style={styles.text}> {this.props.text} </Text>
 			</View>
 		);
 	}
 
 }
+
+const styles = StyleSheet.create({
+	overall:{
+		marginLeft: 15,
+		marginRight: 15,
+	},
+  name:{
+		color: "#1b3324",
+		paddingTop: 10,
+		fontSize: 15,
+		fontWeight: 'bold',
+	},
+	text:{
+		paddingTop: 5,
+		fontSize: 15,
+		borderBottomColor: 'black',
+		borderBottomWidth: 2,
+		borderStyle: 'solid',
+		paddingLeft: 10,
+		paddingRight: 10,
+		paddingBottom: 5,
+	},
+});
